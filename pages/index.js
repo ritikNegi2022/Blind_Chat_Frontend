a73cdc87c7aea2df57911f50c474c5220ab272d7
ritikNegi2022 <negiritik2022@gmail.com>
2023-01-24 13:19:49 +0530

    ''

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import classes from "../styles/Home.module.scss";
 import io from "socket.io-client";
 import ChatBox from "@/components/ChatBox";
-import Head from "next/head";
 
 const endpoint = "https://blind-chat-backend.onrender.com";
 const socket = io(endpoint, { autoConnect: false });
@@ -44,9 +43,6 @@ export default function Home() {
   };
   return (
     <>
-      <Head>
-        <title>Blind Chat - {state}</title>
-      </Head>
       <div className={classes.container}>
         <div className={classes.dashboard}>
           <div className={classes.topContainer}>
@@ -61,13 +57,9 @@ export default function Home() {
           </div>
           {partner && <ChatBox socket={socket} partner={partner} />}
           <div className={classes.buttonContainer}>
-            {(state === "Ideal") & (state !== "Connected") ? (
+            {!partner && (
               <button className={classes.buttonOne} onClick={joinHandler}>
                 Join
-              </button>
-            ) : (
-              <button className={classes.buttonOne} onClick={leaveHandler}>
-                Leave
               </button>
             )}
           </div>
