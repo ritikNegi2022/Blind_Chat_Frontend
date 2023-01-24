6a87b83cb8b4bde36161398c1a064f3bd854d094
ritikNegi2022 <negiritik2022@gmail.com>
2023-01-24 12:07:55 +0530

    ''

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import Header from '@/components/Header';
 import '@/styles/globals.css'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Blind Chat</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
